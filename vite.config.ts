78ad27e5e28e2702ee9844e08da56dce9968dae1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-02 20:40:48 -0600

    add inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { vitePlugin as remix } from "@remix-run/dev"
 import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
+import inspect from "vite-plugin-inspect"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 installGlobals()
 
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths()],
+	plugins: [remix(), tsconfigPaths(), inspect()],
 	server: {
 		fs: {
 			// Restrict files that could be served by Vite's dev server.  Accessing
