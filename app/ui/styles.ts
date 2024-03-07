adbeeccf3cbef2e406d9157a832e9437babe7dfe
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-07 00:00:45 -0600

    lint fix

--- app/ui/styles.ts
+++ app/ui/styles.ts
@@ -1,4 +1,4 @@
-import { ClassNameValue, twMerge } from "tailwind-merge"
+import { type ClassNameValue, twMerge } from "tailwind-merge"
 
 export const panel = (...classes: ClassNameValue[]) =>
 	twMerge("rounded border border-primary-300 bg-primary-200", classes)
