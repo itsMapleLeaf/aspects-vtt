c593a5decb65531b12be7425ab64aa40a5b7ada6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-13 11:08:25 -0500

    add prettier jsdoc plugin

--- app/common/types.ts
+++ app/common/types.ts
@@ -1,20 +1,13 @@
-/**
- * Like omit, but ensures the keys exist in the target object,
- * and preserves union types
- */
+/** Like omit, but ensures the keys exist in the target object, and preserves union types */
 export type StrictOmit<T, K extends keyof T> =
 	T extends object ? Simplify<Pick<T, Exclude<keyof T, K>>> : T
 
-/**
- * Merge two types, overwriting keys in A with keys in B
- */
+/** Merge two types, overwriting keys in A with keys in B */
 export type Overwrite<A extends object, B extends object> = Simplify<
 	B & StrictOmit<A, Extract<keyof B, keyof A>>
 >
 
-/**
- * Simplify complex types into a flat object
- */
+/** Simplify complex types into a flat object */
 export type Simplify<T extends object> = { [K in keyof T]: T[K] } & {}
 
 /** Represents all JS falsy values */
