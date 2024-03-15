167c97c9bc619f93d388c658cb6c3651ed87f8ff
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-15 01:35:20 -0500

    better character gen

--- app/features/characters/CreateCharacterButton.tsx
+++ app/features/characters/CreateCharacterButton.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from "convex/react"
 import * as Lucide from "lucide-react"
+import { randomItem } from "#app/common/random.js"
 import { useCurrentCharacterId } from "#app/features/characters/useCurrentCharacterId.ts"
 import { Button } from "#app/ui/Button.tsx"
 import { api } from "#convex/_generated/api.js"
-import { CHARACTER_FIELDS } from "./characterFields.tsx"
+import { characterNames } from "./characterNames.ts"
 
 export function CreateCharacterButton({
 	roomSlug,
@@ -17,13 +18,28 @@ export function CreateCharacterButton({
 	return (
 		<Button
 			icon={<Lucide.Plus />}
-			title="New Character"
+			title="Add Character"
 			onClick={async () => {
+				const dice: [number, number, number, number, number] = [4, 6, 8, 12, 20]
+				dice.sort()
+				const [strength, sense, mobility, intellect, wit] = dice
+
 				const id = await create({
 					roomSlug,
 					player: username,
-					fields: CHARACTER_FIELDS.flatMap((field) => field.initialValues()),
+					fields: [
+						{ key: "name", value: randomItem(characterNames) ?? "Cute Felirian" },
+						{ key: "health", value: strength * 2 },
+						{ key: "health:max", value: strength * 2 },
+						{ key: "fatigue", value: 0 },
+						{ key: "strength", value: strength },
+						{ key: "sense", value: sense },
+						{ key: "mobility", value: mobility },
+						{ key: "intellect", value: intellect },
+						{ key: "wit", value: wit },
+					],
 				})
+
 				setCurrentCharacterId(id)
 			}}
 		/>
