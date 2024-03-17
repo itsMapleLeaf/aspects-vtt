3eaa3c7ddc8c63df48bde1db289291b97fe1d59d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-17 02:02:33 -0500

    add redirectUrl

--- app/routes/__auth.sign-in.tsx
+++ app/routes/__auth.sign-in.tsx
@@ -2,5 +2,5 @@ import { SignIn } from "@clerk/remix"
 import { $path } from "remix-routes"
 
 export default function SignInRoute() {
-	return <SignIn signUpUrl={$path("/sign-up")} />
+	return <SignIn signUpUrl={$path("/sign-up")} redirectUrl="/" />
 }
