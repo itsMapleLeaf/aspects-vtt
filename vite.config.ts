70a92a866b5aac341fd3a80a706110c85a0b21ee
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-20 23:08:53 -0500

    hacky fix for clerk fetch globals

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev"
-import { installGlobals } from "@remix-run/node"
 import { vercelPreset } from "@vercel/remix/vite"
 import { remixRoutes } from "remix-routes/vite.js"
 import { visualizer } from "rollup-plugin-visualizer"
-import { defineConfig } from "vite"
+import { type Plugin, defineConfig } from "vite"
 import inspect from "vite-plugin-inspect"
-
-installGlobals()
 
 export default defineConfig({
 	plugins: [
@@ -16,10 +13,34 @@ export default defineConfig({
 				v3_relativeSplatPath: true,
 				v3_throwAbortReason: true,
 			},
-			presets: [process.env.VERCEL && vercelPreset()].filter(Boolean),
+			presets: [vercelPreset()].filter(Boolean),
 		}),
 		remixRoutes(),
 		inspect(),
 		visualizer({ emitFile: true, filename: "build/stats.html" }),
+		stupidDumbHackyClerkFix(),
 	],
 })
+
+function stupidDumbHackyClerkFix(): Plugin {
+	return {
+		name: "remix-globals",
+		config(config, env) {
+			if (env.isSsrBuild) {
+				config.build ??= {}
+				config.build.rollupOptions ??= {}
+				const output = [(config.build.rollupOptions.output ??= [])].flat()
+				output.push({
+					banner: `import { installGlobals } from "@remix-run/node";installGlobals();`,
+				})
+
+				config.ssr ??= {}
+				if (Array.isArray(config.ssr.noExternal)) {
+					config.ssr.noExternal.push("@clerk")
+				} else {
+					config.ssr.noExternal = ["@clerk"]
+				}
+			}
+		},
+	}
+}
