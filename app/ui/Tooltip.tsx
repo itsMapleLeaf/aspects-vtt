bf5b5eb561717ccd5d6def6787e1df5037ae108f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-23 03:25:28 -0500

    improve tooltip transition logic

--- app/ui/Tooltip.tsx
+++ app/ui/Tooltip.tsx
@@ -12,31 +12,11 @@ interface TooltipProps extends ComponentPropsWithoutRef<"button"> {
 }
 
 export function Tooltip({ text, placement, ...props }: TooltipProps) {
-	const buttonId = useId()
-	const tooltipId = useId()
 	const [hover, setHover] = useState(false)
 	const [focus, setFocus] = useState(false)
-	const visible = hover || focus
-
-	const [transitionState, setTransitionState] = useState<
-		"willEnter" | "entered" | "willExit" | "exited"
-	>("exited")
-	const enter = transitionState === "entered"
-	const mounted = transitionState !== "exited"
-
-	useEffect(() => {
-		if (visible && (transitionState === "exited" || transitionState === "willExit")) {
-			setTransitionState("willEnter")
-		}
-		if (transitionState === "willEnter") {
-			setTimeout(() => {
-				setTransitionState("entered")
-			})
-		}
-		if (transitionState === "entered" && !visible) {
-			setTransitionState("willExit")
-		}
-	}, [visible, transitionState])
+	const transition = useTransitionState(hover || focus)
+	const buttonId = useId()
+	const tooltipId = useId()
 
 	return (
 		<FloatingProvider placement={placement}>
@@ -66,18 +46,14 @@ export function Tooltip({ text, placement, ...props }: TooltipProps) {
 					}}
 				/>
 			</FloatingReference>
-			{mounted && (
+			{transition.mounted && (
 				<Floating className="pointer-events-none">
 					<div
 						id={tooltipId}
 						role="tooltip"
-						aria-expanded={enter}
+						aria-expanded={transition.enter}
 						className="w-fit max-w-32 translate-y-1 rounded bg-white px-2 py-1 text-center text-xs font-semibold text-primary-100 opacity-0 shadow-md shadow-black/50 transition aria-expanded:translate-y-0 aria-expanded:opacity-100"
-						onTransitionEnd={() => {
-							if (transitionState === "willExit") {
-								setTransitionState("exited")
-							}
-						}}
+						ref={transition.ref}
 					>
 						{text}
 					</div>
@@ -85,4 +61,62 @@ export function Tooltip({ text, placement, ...props }: TooltipProps) {
 			)}
 		</FloatingProvider>
 	)
+}
+
+function useTransitionState(visible: boolean) {
+	const [transitionState, setTransitionState] = useState<
+		"willEnter" | "entered" | "exiting" | "exited"
+	>("exited")
+
+	const [element, ref] = useState<HTMLElement | null>(null)
+
+	useEffect(() => {
+		switch (transitionState) {
+			case "exited": {
+				if (visible) setTransitionState("willEnter")
+				break
+			}
+
+			case "willEnter": {
+				const id = setTimeout(() => {
+					setTransitionState((s) => (s === "willEnter" ? "entered" : s))
+				})
+				return () => clearTimeout(id)
+			}
+
+			case "entered": {
+				if (!visible) setTransitionState("exiting")
+				break
+			}
+
+			case "exiting": {
+				if (visible) {
+					setTransitionState("willEnter")
+					return
+				}
+
+				if (element) {
+					const handleTransitionEnd = () => {
+						setTransitionState("exited")
+					}
+					element.addEventListener("transitionend", handleTransitionEnd)
+					element.addEventListener("transitioncancel", handleTransitionEnd)
+					const id = setTimeout(handleTransitionEnd, 500) // Fallback in case transitionend doesn't fire
+					return () => {
+						element.removeEventListener("transitionend", handleTransitionEnd)
+						element.removeEventListener("transitioncancel", handleTransitionEnd)
+						clearTimeout(id)
+					}
+				}
+
+				break
+			}
+		}
+	}, [element, transitionState, visible])
+
+	return {
+		mounted: transitionState !== "exited",
+		enter: transitionState === "entered",
+		ref,
+	}
 }
