698cd7ac86fb283b8ff36d56d68c02080b68fa39
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-23 02:48:01 -0500

    darker panel style

--- app/ui/styles.ts
+++ app/ui/styles.ts
@@ -1,4 +1,4 @@
 import { type ClassNameValue, twMerge } from "tailwind-merge"
 
 export const panel = (...classes: ClassNameValue[]) =>
-	twMerge("rounded border border-primary-300 bg-primary-200", classes)
+	twMerge("rounded border border-primary-300 bg-primary-200/50", classes)
