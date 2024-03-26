d8dbe0806ece0bcd5f3e74452e8e9f4e11e0245f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-26 13:01:35 -0500

    expanding text areas

--- app/ui/Input.tsx
+++ app/ui/Input.tsx
@@ -1,52 +1,36 @@
-import { omit } from "#app/common/object.js"
-import type { Overwrite } from "#app/common/types.ts"
 import type React from "react"
 import type { ComponentPropsWithoutRef, ReactNode } from "react"
-import { twMerge } from "tailwind-merge"
+import { type ClassNameValue, twMerge } from "tailwind-merge"
 import { panel } from "./styles.ts"
 
-export type InputProps = Overwrite<
-	| (ComponentPropsWithoutRef<"input"> & {
-			multiline?: false | undefined
-			elementRef?: React.Ref<HTMLInputElement>
-	  })
-	| (ComponentPropsWithoutRef<"textarea"> & {
-			multiline: true
-			elementRef?: React.Ref<HTMLTextAreaElement>
-	  }),
-	{
-		icon?: ReactNode
-		align?: "left" | "right" | "center"
-	}
->
+export interface InputProps extends ComponentPropsWithoutRef<"input">, InputStyleProps {
+	elementRef?: React.Ref<HTMLInputElement>
+	icon?: ReactNode
+}
 
-export function Input({ icon, className, align, ...props }: InputProps) {
-	const inputClassNameBase = panel(
-		twMerge(
-			"w-full min-w-0 rounded border border-primary-300 bg-primary-200 pl-8 pr-3 transition peer-empty:pl-3",
-			align === "left" && "text-left",
-			align === "right" && "text-right",
-			align === "center" && "text-center",
-		),
-	)
+export function Input({ icon, className, align, elementRef, ...props }: InputProps) {
 	return (
 		<div className={twMerge("group relative flex w-full items-center", className)}>
 			<div className="peer pointer-events-none absolute left-2 opacity-50 transition *:size-5 group-focus-within:opacity-100">
 				{icon}
 			</div>
-			{props.multiline ?
-				<textarea
-					rows={3}
-					{...omit(props, ["multiline", "elementRef"])}
-					className={twMerge(inputClassNameBase, "py-1.5 leading-7")}
-					ref={props.elementRef}
-				/>
-			:	<input
-					{...omit(props, ["multiline", "elementRef"])}
-					className={twMerge(inputClassNameBase, "h-10")}
-					ref={props.elementRef}
-				/>
-			}
+			<input {...props} className={inputStyle({ align }, "h-10")} ref={elementRef} />
 		</div>
+	)
+}
+
+export interface InputStyleProps {
+	align?: "left" | "right" | "center"
+}
+
+export function inputStyle({ align }: InputStyleProps, ...classes: ClassNameValue[]) {
+	return panel(
+		twMerge(
+			"w-full min-w-0 rounded border border-primary-300 bg-primary-200 pl-8 pr-3 transition peer-empty:pl-3",
+			align === "left" && "text-left",
+			align === "right" && "text-right",
+			align === "center" && "text-center",
+			...classes,
+		),
 	)
 }
