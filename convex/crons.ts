c517bbc2648c3267da5b8fcf06f43d2fe19b4fab
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-04-06 02:36:32 -0500

    do weekly cron instead of daily

--- convex/crons.ts
+++ convex/crons.ts
@@ -2,5 +2,9 @@ import { cronJobs } from "convex/server"
 import { internal } from "./_generated/api"
 
 const crons = cronJobs()
-crons.daily("import notion docs", { hourUTC: 17, minuteUTC: 0 }, internal.notionImports.importData)
+crons.weekly(
+	"import notion docs",
+	{ dayOfWeek: "friday", hourUTC: 17, minuteUTC: 0 },
+	internal.notionImports.importData,
+)
 export default crons
