b987071daff91a422c9dfb58aef516ccfbd28896
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-04-20 23:12:41 -0500

    oh, so that's how you do it

--- app/common/random.test.ts
+++ app/common/random.test.ts
@@ -1,12 +1,11 @@
 import { describe, expect, it } from "bun:test"
-import "iterator-helpers-polyfill"
+import { Iterator } from "iterator-helpers-polyfill"
 import { randomItem } from "./random.ts"
-import { range } from "./range.ts"
 
 describe("randomItem", () => {
 	it("should return every item at least once when run multiple times", () => {
 		const items = ["a", "b", "c"]
-		const randomItems = new Set(range(1000).map(() => randomItem(items)))
+		const randomItems = new Set(Iterator.range(1000).map(() => randomItem(items)))
 		expect(randomItems.size).toBe(items.length)
 	})
 })
