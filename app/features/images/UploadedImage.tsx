e54b92da9564f3faeb532bcf5be742298a2440a8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-04-23 21:59:27 -0500

    image should always fill area

--- app/features/images/UploadedImage.tsx
+++ app/features/images/UploadedImage.tsx
@@ -17,14 +17,9 @@ export function UploadedImage({ id, emptyIcon, ...props }: UploadedImageProps) {
 	return (
 		<div {...withMergedClassName(props, "flex items-center justify-center")}>
 			{imageUrl ? (
-				<img
-					src={imageUrl}
-					alt=""
-					className="size-auto max-h-full max-w-full rounded object-contain"
-					draggable={false}
-				/>
+				<img src={imageUrl} alt="" className="size-full object-contain" draggable={false} />
 			) : (
-				<div className="flex size-full flex-col text-primary-600 opacity-50 *:m-auto *:aspect-square *:size-full *:max-h-24 *:max-w-24 empty:hidden">
+				<div className="flex size-full flex-col p-2 text-primary-600 opacity-50 *:m-auto *:aspect-square *:size-full empty:hidden">
 					{emptyIcon}
 				</div>
 			)}
