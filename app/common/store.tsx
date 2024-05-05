b8e05953675ce0b11f7a10f9b42f5b7b399b0b50
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-05-05 00:04:03 -0500

    add getState function to store

--- app/common/store.tsx
+++ app/common/store.tsx
@@ -1,16 +1,29 @@
 import * as React from "react"
+import { useEffectEvent } from "./react.ts"
 
 export function createStore<State, Actions, ContextValue>(options: {
 	state: State
-	actions: (setState: React.Dispatch<React.SetStateAction<State>>) => Actions
+	actions: (setState: React.Dispatch<React.SetStateAction<State>>, getState: () => State) => Actions
 	context: (state: State) => ContextValue
 }) {
 	const StateContext = React.createContext(options.context(options.state))
-	const ActionsContext = React.createContext(options.actions(() => {}))
+
+	const ActionsContext = React.createContext(
+		options.actions(
+			() => {},
+			() => options.state,
+		),
+	)
 
 	function Provider({ children }: { children: React.ReactNode }) {
 		const [state, setState] = React.useState(options.state)
-		const actions = React.useMemo(() => options.actions(setState), [options.actions])
+		const getState = useEffectEvent(() => state)
+
+		const actions = React.useMemo(
+			() => options.actions(setState, getState),
+			[options.actions, getState],
+		)
+
 		return (
 			<StateContext.Provider value={options.context(state)}>
 				<ActionsContext.Provider value={actions}>{children}</ActionsContext.Provider>
