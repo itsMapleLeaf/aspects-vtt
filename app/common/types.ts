b57c501c5d218496347cc227d2815fa1aecb6c22
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-05-08 13:27:49 -0500

    add object constraint on StrictOmit

--- app/common/types.ts
+++ app/common/types.ts
@@ -1,7 +1,5 @@
 /** Like omit, but ensures the keys exist in the target object, and preserves union types */
-export type StrictOmit<T, K extends keyof T> = T extends object
-	? Simplify<Pick<T, Exclude<keyof T, K>>>
-	: T
+export type StrictOmit<T extends object, K extends keyof T> = Simplify<Pick<T, Exclude<keyof T, K>>>
 
 /** Merge two types, overwriting keys in A with keys in B */
 export type Overwrite<A extends object, B extends object> = Simplify<
