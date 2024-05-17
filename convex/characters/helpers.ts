451bbbff75293e3c568102d9ce016e97681cf89c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-05-16 19:01:19 -0500

    count room owner as character owner

--- convex/characters/helpers.ts
+++ convex/characters/helpers.ts
@@ -1,17 +1,18 @@
-import { Effect, pipe } from "effect"
+import { Effect } from "effect"
 import type { Id } from "../_generated/dataModel"
 import { UnauthorizedError, getUserFromIdentityEffect } from "../auth/helpers.ts"
 import { queryDoc } from "../helpers/effect.ts"
 
 export function ensureViewerOwnsCharacter(characterId: Id<"characters">) {
-	return pipe(
-		Effect.all({
+	return Effect.gen(function* () {
+		const { user, character } = yield* Effect.all({
 			user: getUserFromIdentityEffect(),
 			character: queryDoc((ctx) => ctx.table("characters").get(characterId).doc()),
-		}),
-		Effect.filterOrFail(
-			({ user, character }) => user.clerkId === character.playerId,
-			() => new UnauthorizedError(),
-		),
-	)
+		})
+		const room = yield* queryDoc((ctx) => ctx.table("rooms").get(character.roomId).doc())
+		if (room.ownerId === user.clerkId || character.playerId === user.clerkId) {
+			return { user, character, room }
+		}
+		yield* Effect.fail(new UnauthorizedError())
+	})
 }
