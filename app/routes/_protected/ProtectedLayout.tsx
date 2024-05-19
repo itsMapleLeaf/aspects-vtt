96072e8a5277b6e8186f27f439d831e4d96a4066
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-05-18 19:38:04 -0500

    show loading spinner for entire thing

--- app/routes/_protected/ProtectedLayout.tsx
+++ app/routes/_protected/ProtectedLayout.tsx
@@ -1,4 +1,4 @@
-import { ClerkLoading, SignInButton, SignUpButton } from "@clerk/remix"
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton } from "@clerk/remix"
 import { useHref, useLocation } from "@remix-run/react"
 import { AuthLoading, Authenticated, Unauthenticated } from "convex/react"
 import * as Lucide from "lucide-react"
@@ -25,25 +25,27 @@ function UnauthenticatedMessage() {
 	const currentUrl = useHref(useLocation())
 	return (
 		<AppHeaderLayout>
-			<main>
-				<EmptyStatePanel
-					icon={<Lucide.Lock />}
-					message="You must be signed in to continue."
-					actions={
-						<>
-							<ClerkLoading>
-								<Loading size="sm" />
-							</ClerkLoading>
-							<SignInButton mode="modal" forceRedirectUrl={currentUrl}>
-								<Button icon={<Lucide.LogIn />} text="Sign in" />
-							</SignInButton>
-							<SignUpButton mode="modal" forceRedirectUrl={currentUrl}>
-								<Button icon={<Lucide.UserPlus />} text="Create account" />
-							</SignUpButton>
-						</>
-					}
-				/>
-			</main>
+			<ClerkLoading>
+				<Loading />
+			</ClerkLoading>
+			<ClerkLoaded>
+				<main>
+					<EmptyStatePanel
+						icon={<Lucide.Lock />}
+						message="You must be signed in to continue."
+						actions={
+							<>
+								<SignInButton mode="modal" forceRedirectUrl={currentUrl}>
+									<Button icon={<Lucide.LogIn />} text="Sign in" />
+								</SignInButton>
+								<SignUpButton mode="modal" forceRedirectUrl={currentUrl}>
+									<Button icon={<Lucide.UserPlus />} text="Create account" />
+								</SignUpButton>
+							</>
+						}
+					/>
+				</main>
+			</ClerkLoaded>
 		</AppHeaderLayout>
 	)
 }
