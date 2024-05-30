dd7a26aa6849522e23c8d02b86038a938a1bbdf0
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-05-30 16:01:01 -0400

    this feels nicer

--- convex/auth/functions.ts
+++ convex/auth/functions.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect"
+import { Effect, pipe } from "effect"
 import { effectMutation, withMutationCtx } from "../helpers/effect.ts"
 import { type QueryCtx, query } from "../helpers/ents.ts"
 import { getIdentityEffect, getUserFromClerkId, getUserFromIdentity } from "./helpers.ts"
@@ -12,29 +12,29 @@ export const user = query({
 export const setup = effectMutation({
 	args: {},
 	handler() {
-		return Effect.gen(function* () {
-			const identity = yield* getIdentityEffect()
-			const data = {
-				name: identity.nickname || identity.name || "anonymous",
-				avatarUrl: identity.pictureUrl,
-			}
-
-			return yield* getUserFromClerkId(identity.subject).pipe(
-				Effect.flatMap((user) => {
-					return withMutationCtx(async (ctx) => {
-						return await ctx.table("users").getX(user._id).patch(data).get().doc()
-					})
-				}),
-				Effect.catchTag("UserNotFoundError", () => {
-					return withMutationCtx(async (ctx) => {
-						return await ctx
-							.table("users")
-							.insert({ ...data, clerkId: identity.subject })
-							.get()
-							.doc()
-					})
-				}),
-			)
-		})
+		return pipe(
+			getIdentityEffect(),
+			Effect.flatMap((identity) => {
+				const data = {
+					name: identity.nickname || identity.name || "anonymous",
+					avatarUrl: identity.pictureUrl,
+				}
+				return pipe(
+					getUserFromClerkId(identity.subject),
+					Effect.flatMap((user) =>
+						withMutationCtx((ctx) => ctx.table("users").getX(user._id).patch(data).get().doc()),
+					),
+					Effect.catchTag("UserNotFoundError", () =>
+						withMutationCtx((ctx) =>
+							ctx
+								.table("users")
+								.insert({ ...data, clerkId: identity.subject })
+								.get()
+								.doc(),
+						),
+					),
+				)
+			}),
+		)
 	},
 })
