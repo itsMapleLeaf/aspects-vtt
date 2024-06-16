f713d9c15d6cabd81afd67bfc1585237e3375322
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-15 23:01:22 -0500

    some helpers i might need later

--- app/features/characters/helpers.ts
+++ app/features/characters/helpers.ts
@@ -1,3 +1,6 @@
+import type { Id } from "../../../convex/_generated/dataModel"
+import type { Attribute } from "../../../data/attributes.ts"
+import { statDiceKinds, statDiceKindsByName, type DiceKind } from "../dice/diceKinds.tsx"
 import type { ApiCharacter } from "./types.ts"
 
 export function getThresholds(character: ApiCharacter) {
@@ -6,4 +9,13 @@ export function getThresholds(character: ApiCharacter) {
 		fatigue:
 			character.sense + character.intellect + character.wit + character.fatigueThresholdDelta,
 	}
+}
+
+export function formatMention(character: { _id: Id<"characters"> }) {
+	return `<@${character._id}>`
+}
+
+export function getAttributeDiceKind(character: ApiCharacter, attribute: Attribute): DiceKind {
+	const kind = statDiceKinds.find((kind) => kind.faces.length === character[attribute.id])
+	return kind ?? statDiceKindsByName.d4
 }
