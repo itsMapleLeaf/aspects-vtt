6e1ca7e019eef9c3d89e83d6cd199ea2b94166ff
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-16 22:33:56 -0500

    update general skills

--- data/generalSkills.ts
+++ data/generalSkills.ts
@@ -10,11 +10,7 @@ const generalSkills = {
 		description: `Roll Mobility and add that to your movement amount this turn.`,
 	},
 	defend: {
-		description: `Do not take 1d4 fatigue when using this skill.
-
-Enter **defending** status. Leave defending status when taking damage, or after making another action.
-
-When you take damage while defending, take 1d4 fatigue, then roll Strength and reduce it by that amount, to no less than 1.`,
+		description: `You can only use this as a reaction. The next time you take damage, roll Strength and reduce it by that amount to no less than 1.`,
 	},
 	dodge: {
 		description: `You may only use this as a reaction.
@@ -22,10 +18,10 @@ When you take damage while defending, take 1d4 fatigue, then roll Strength and r
 Before you take damage, roll Mobility. If the effect is higher than the damage amount, prevent all damage. If necessary, you must make a movement out of the damage area.`,
 	},
 	focus: {
-		description: `Add an extra die to your next attribute roll.`,
+		description: `Your next attribute roll has +1 boost die.`,
 	},
 	intimidate: {
-		description: `Choose a visible target within 20 meters. Roll Wit.
+		description: `Choose a visible target within 5 meters. Roll Wit.
 
 The target must make an equal or higher Sense or Wit roll. If they fail, they take 1d4 fatigue and cannot approach or target you until the start of your next turn.`,
 	},
@@ -38,7 +34,7 @@ The target must make an equal or higher Sense or Wit roll. If they fail, they ta
 They cannot move until you use an action to release them, leave their area, or until they use an action to win a contested strength roll against you.`,
 	},
 	strike: {
-		description: `Choose a die no higher than your strength. Roll that die twice, and deal the total to a nearby character.`,
+		description: `Choose a die no higher than your strength. Roll that die twice and deal damage equal to the total to a nearby character.`,
 	},
 	taunt: {
 		description: `Target a visible character no more than 20 meters away. Roll Wit. If the effect meets or exceeds the character’s Wit or Intellect (the higher of the two), their next targeted action must target you.`,
