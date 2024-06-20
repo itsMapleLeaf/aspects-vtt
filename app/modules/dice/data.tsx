f60d8a506e81ba0e6f536678df8a9217d65fbf0f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-19 22:41:34 -0500

    update and clean up dice data

--- app/modules/dice/data.tsx
+++ app/modules/dice/data.tsx
@@ -1,23 +1,15 @@
 import { Iterator } from "iterator-helpers-polyfill"
 import * as Lucide from "lucide-react"
+import type React from "react"
 import { twMerge } from "tailwind-merge"
 import type { DiceInput } from "../../../convex/messages/types.ts"
-import { Tooltip } from "../../ui/Tooltip.old.tsx"
+import { Tooltip } from "../../ui/Tooltip.tsx"
 
 export interface DiceStat {
 	name: string
 	min?: number
 	className?: string
 }
-
-const effectStat = {
-	name: "Effect",
-	min: 1,
-	className: twMerge("text-primary-800"),
-}
-const boostStat = { name: "Boost", className: twMerge("text-green-400") }
-const snagStat = { name: "Snag", className: twMerge("text-red-400") }
-export const diceStats: DiceStat[] = [effectStat, boostStat, snagStat]
 
 export interface DiceKind {
 	name: string
@@ -30,6 +22,25 @@ export interface DiceFace {
 	modifyStats: ReadonlyMap<DiceStat, number>
 	render: () => React.ReactElement
 }
+
+const effectStat = {
+	name: "Effect",
+	min: 1,
+	className: twMerge("text-primary-800"),
+} satisfies DiceStat
+const baseStat = {
+	name: "Base",
+	className: twMerge("text-blue-400"),
+} satisfies DiceStat
+const boostStat = {
+	name: "Boost",
+	className: twMerge("text-green-400"),
+} satisfies DiceStat
+const snagStat = {
+	name: "Snag",
+	className: twMerge("text-red-400"),
+} satisfies DiceStat
+export const diceStats: DiceStat[] = [effectStat, baseStat, boostStat, snagStat]
 
 export function getDiceKindApiInput(kind: DiceKind, count: number): DiceInput {
 	return {
@@ -54,14 +65,14 @@ export const statDiceKindsByName = {
 		faceCount: 8,
 		renderIcon: () => <Lucide.Diamond />,
 	}),
+	d10: defineNumeric({
+		faceCount: 8,
+		renderIcon: () => <Lucide.Hexagon />,
+	}),
 	d12: defineNumeric({
-		faceCount: 12,
+		faceCount: 10,
 		renderIcon: () => <Lucide.Pentagon />,
 		textClassName: twMerge("translate-y-[2px]"),
-	}),
-	d20: defineNumeric({
-		faceCount: 20,
-		renderIcon: () => <Lucide.Hexagon />,
 	}),
 }
 
@@ -106,36 +117,32 @@ function defineNumeric({
 	return {
 		name,
 		render: () => (
-			<div className="flex-center-col relative text-primary-700 @container">
-				<div className="size-full *:size-full *:fill-primary-200 *:stroke-1">{renderIcon()}</div>
-				<p
-					className={twMerge(
-						"absolute hidden text-[length:28cqw] font-semibold @[3rem]:block",
-						textClassName,
-					)}
-				>
-					d{faceCount}
-				</p>
-			</div>
+			<Diecon
+				shape={renderIcon()}
+				symbol={
+					<p className={twMerge("text-[length:28cqw] font-semibold", textClassName)}>
+						d{faceCount}
+					</p>
+				}
+				tooltip={name}
+			/>
 		),
 		faces: Iterator.range(1, faceCount + 1)
 			.map((n) => ({
 				render: () => (
-					<Tooltip
-						text={`${name}: ${n}`}
-						placement="top"
-						className="flex-center-col relative transition @container *:pointer-events-none hover:brightness-150 data-[max=true]:text-primary-700"
-						data-max={n === faceCount}
-					>
-						<div className="size-full *:size-full *:fill-primary-200 *:stroke-1">
-							{renderIcon()}
-						</div>
-						<p className={twMerge("absolute text-[length:36cqw] font-semibold", textClassName)}>
-							{n}
-						</p>
-					</Tooltip>
+					<Diecon
+						shape={renderIcon()}
+						symbol={
+							<p className={twMerge("text-[length:36cqw] font-semibold", textClassName)}>{n}</p>
+						}
+						tooltip={`${name}: ${n}`}
+						className={n === faceCount ? "text-primary-700" : ""}
+					/>
 				),
-				modifyStats: new Map([[effectStat, n]]),
+				modifyStats: new Map([
+					[effectStat, n],
+					[baseStat, n],
+				]),
 			}))
 			.toArray(),
 	}
@@ -154,16 +161,16 @@ function defineModifier({
 	multiplier: number
 	stat: DiceStat
 }): DiceKind {
-	const faceCount = 4
+	const faceCount = 6
 	return {
 		name,
 		render: () => (
-			<div className={twMerge("flex-center-col relative", className)}>
-				<Lucide.Triangle className="size-full fill-primary-200 stroke-1" />
-				<div className="absolute hidden size-[50%] translate-y-[3px] *:size-full @[3rem]:block">
-					{renderIcon()}
-				</div>
-			</div>
+			<Diecon
+				shape={<Lucide.Square />}
+				symbol={renderIcon()}
+				tooltip={name}
+				className={className}
+			/>
 		),
 		faces: Iterator.range(1, faceCount + 1)
 			.map((face) => ({
@@ -172,19 +179,40 @@ function defineModifier({
 					[stat, face],
 				]),
 				render: () => (
-					<Tooltip
-						text={`${name}: ${face}`}
-						placement="top"
-						className={twMerge(
-							"flex-center-col relative transition @container [--icon-gap:9px] *:pointer-events-none hover:brightness-150",
-							className,
-						)}
-					>
-						<Lucide.Triangle className="size-full fill-primary-200 stroke-1" />
-						<p className="absolute translate-y-[3px] text-[length:36cqw] font-semibold">{face}</p>
-					</Tooltip>
+					<Diecon
+						shape={<Lucide.Square />}
+						symbol={<p className="text-[length:36cqw] font-semibold">{face}</p>}
+						tooltip={`${name}: ${face}`}
+						className={className}
+					/>
 				),
 			}))
 			.toArray(),
 	}
+}
+
+function Diecon({
+	shape,
+	symbol,
+	tooltip,
+	className,
+}: {
+	shape: React.ReactNode
+	symbol: React.ReactNode
+	tooltip: React.ReactNode
+	className?: string
+}) {
+	return (
+		<Tooltip content={tooltip}>
+			<div
+				className={twMerge(
+					"flex-center-col relative size-full cursor-default transition @container hover:brightness-150",
+					className,
+				)}
+			>
+				<div className="relative size-full *:size-full *:fill-primary-200 *:stroke-1">{shape}</div>
+				<div className="absolute hidden *:size-full @[3rem]:block">{symbol}</div>
+			</div>
+		</Tooltip>
+	)
 }
