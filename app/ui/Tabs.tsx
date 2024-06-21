75ac9bbed40b172b4a23b63dea91d43764a3a9ef
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-21 00:57:37 -0500

    tabs: use startTransition

--- app/ui/Tabs.tsx
+++ app/ui/Tabs.tsx
@@ -1,4 +1,5 @@
 import * as Ariakit from "@ariakit/react"
+import { startTransition, useId } from "react"
 import { twMerge } from "tailwind-merge"
 
 export function Tabs(props: Ariakit.TabProviderProps) {
@@ -13,6 +14,8 @@ function List(props: Ariakit.TabListProps) {
 Tabs.List = List
 
 function Tab(props: Ariakit.TabProps) {
+	const tabs = Ariakit.useTabContext()
+	const fallbackId = useId()
 	return (
 		<Ariakit.Tab
 			{...props}
@@ -20,12 +23,18 @@ function Tab(props: Ariakit.TabProps) {
 				"h-10 flex-1 rounded px-3 opacity-50 transition hover:bg-primary-200 aria-selected:bg-primary-200 aria-selected:opacity-100",
 				props.className,
 			)}
+			onClick={(event) => {
+				startTransition(() => {
+					props.onClick?.(event)
+					tabs?.select(props.id || fallbackId)
+				})
+			}}
 		/>
 	)
 }
 Tabs.Tab = Tab
 
 function Panel(props: Ariakit.TabPanelProps) {
-	return <Ariakit.TabPanel {...props} />
+	return <Ariakit.TabPanel unmountOnHide {...props} />
 }
 Tabs.Panel = Panel
