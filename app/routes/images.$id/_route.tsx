ea9e1c997f73847f0545beaf53b2273705b514d8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-22 22:27:36 -0500

    cache images from proxy

--- app/routes/images.$id/_route.tsx
+++ app/routes/images.$id/_route.tsx
@@ -6,5 +6,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 	const { id } = $params("/images/:id", params)
 	const url = new URL("/image", clientEnv.VITE_CONVEX_URL.replace(/\.cloud\/*$/, ".site"))
 	url.searchParams.set("id", id)
-	return fetch(url.href, request)
+	const response = await fetch(url.href, request)
+	response.headers.set("Cache-Control", "public, max-age=31536000")
+	return response
 }
