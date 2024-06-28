91f4858fec8d6d9f592774f52935f17f9902e6f9
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-27 22:08:19 -0500

    new scene button

--- app/modules/scenes/SceneResource.tsx
+++ app/modules/scenes/SceneResource.tsx
@@ -1,20 +1,28 @@
-import { useMutation } from "convex/react"
-import { ConvexError } from "convex/values"
-import { LucideCheck, LucideImage, LucideImagePlay, LucideImagePlus } from "lucide-react"
-import { useActionState, useState } from "react"
+import { useConvex, useMutation } from "convex/react"
+import { LucideDoorOpen, LucideImage, LucideImagePlay, LucideImagePlus } from "lucide-react"
+import React, { useEffect, useState } from "react"
 import { twMerge } from "tailwind-merge"
 import { z } from "zod"
 import { loadImage } from "~/helpers/dom/images.ts"
+import type { Overwrite } from "~/helpers/types.ts"
 import { defineResource, type Resource } from "~/modules/resources/Resource"
 import { Button } from "~/ui/Button.tsx"
-import { FormErrors, FormField, FormLayout, FormRow } from "~/ui/Form.tsx"
-import { Input } from "~/ui/Input.tsx"
-import { Menu, MenuItem } from "~/ui/Menu.tsx"
-import { Modal, useModalContext } from "~/ui/Modal.tsx"
-import { useToaster } from "~/ui/Toaster.tsx"
+import { EditableInput } from "~/ui/EditableInput.tsx"
+import { EditableIntegerInput } from "~/ui/EditableIntegerInput.tsx"
+import { FormField, FormLayout, FormRow } from "~/ui/Form.tsx"
+import { MenuItem } from "~/ui/Menu.tsx"
+import {
+	ModalButton,
+	ModalPanel,
+	ModalPanelContent,
+	ModalProvider,
+	type ModalProviderProps,
+	useModalContext,
+} from "~/ui/Modal.tsx"
 import { api } from "../../../convex/_generated/api"
 import type { Id } from "../../../convex/_generated/dataModel"
 import { ImageUploader } from "../api-images/ImageUploader.tsx"
+import { useMutationAction, useSafeAction } from "../convex/hooks.ts"
 import { useRoom } from "../rooms/roomContext.tsx"
 import type { ApiScene } from "./types.ts"
 
@@ -37,7 +45,9 @@ export const SceneResource = defineResource({
 
 	TreeItem: ({ scene }: { scene: ApiScene }) => <SceneTreeElement scene={scene} />,
 
-	CreateMenuItem: () => <MenuItem icon={<LucideImagePlus />} text="Scene" />,
+	CreateMenuItem: () => (
+		<MenuItem icon={<LucideImagePlus />} text="Scene" render={<NewSceneButton />} />
+	),
 })
 
 function SceneTreeElement({ scene }: { scene: ApiScene }) {
@@ -46,103 +56,121 @@ function SceneTreeElement({ scene }: { scene: ApiScene }) {
 	const [open, setOpen] = useState(false)
 
 	return (
-		<Menu>
-			<Modal
-				text={scene.name}
-				icon={isCurrent ? <LucideImagePlay /> : <LucideImage />}
-				appearance="clear"
-				className={twMerge(
-					"w-full justify-start",
-					isCurrent && "text-primary-800 text-opacity-100",
-				)}
-				title={`Editing ${scene.name}`}
-				open={open}
-				onOpenChange={setOpen}
-			>
-				<SceneEditor scene={scene} />
-			</Modal>
-		</Menu>
+		<SceneEditorModal open={open} setOpen={setOpen} scene={scene}>
+			<SceneEditorModal.Button
+				render={
+					<Button
+						text={scene.name}
+						icon={isCurrent ? <LucideImagePlay /> : <LucideImage />}
+						appearance="clear"
+						className={twMerge(
+							"w-full justify-start",
+							isCurrent &&
+								"bg-primary-800 bg-opacity-10 text-primary-800 text-opacity-100 hover:bg-opacity-25",
+						)}
+					/>
+				}
+			/>
+		</SceneEditorModal>
 	)
 }
+
+function SceneEditorModal({
+	scene,
+	children,
+	...props
+}: { scene: ApiScene } & Overwrite<ModalProviderProps, { children: React.ReactNode }>) {
+	return (
+		<ModalProvider {...props}>
+			{children}
+			<ModalPanel title={`Editing ${scene.name}`}>
+				<ModalPanelContent>
+					<SceneEditor scene={scene} />
+				</ModalPanelContent>
+			</ModalPanel>
+		</ModalProvider>
+	)
+}
+SceneEditorModal.Button = ModalButton
 
 function SceneEditor({ scene }: { scene: ApiScene }) {
 	const room = useRoom()
 	const updateScene = useMutation(api.scenes.functions.update)
-	const updateRoom = useMutation(api.rooms.functions.update)
-	const toast = useToaster()
+	const [updateRoomState, updateRoom] = useMutationAction(api.rooms.functions.update)
 	const isCurrent = room.currentScene === scene._id
 	const modal = useModalContext()
 
-	const [state, action] = useActionState(async (_: unknown, formData: FormData) => {
-		const schema = z.object({
-			name: z.string().min(1).max(100),
-			cellSize: z.coerce.number().int().min(20).max(100),
-		})
-
-		const result = schema.safeParse(Object.fromEntries(formData))
-		if (!result.success) {
-			return { errors: result.error.formErrors }
+	useEffect(() => {
+		if (updateRoomState.type === "success") {
+			modal?.hide()
 		}
-
-		try {
-			await updateScene({
-				id: scene._id,
-				name: result.data.name,
-				cellSize: result.data.cellSize,
-			})
-		} catch (error) {
-			toast.error({
-				title: "Something went wrong :(",
-				body: error instanceof ConvexError ? error.message : undefined,
-			})
-		}
-
-		return {}
-	}, {})
+	}, [modal, updateRoomState.type])
 
 	return (
-		<form className="contents" action={action}>
-			<FormLayout>
-				<FormErrors errors={state.errors?.formErrors} />
-
-				<FormRow>
-					<FormField label="Scene name" className="flex-1">
-						<Input defaultValue={scene.name} />
-						<FormErrors errors={state.errors?.fieldErrors.name} />
-					</FormField>
-					<FormField label="Cell size" className="w-24">
-						<Input align="center" value={scene.cellSize} required pattern="\d+" />
-						<FormErrors errors={state.errors?.fieldErrors.cellSize} />
-					</FormField>
-				</FormRow>
-
-				<ImageUploader
-					imageId={scene.background}
-					onUpload={async (imageId, file) => {
-						const { width, height } = await loadImage(URL.createObjectURL(file))
-						await updateScene({
-							id: scene._id,
-							background: imageId,
-							backgroundDimensions: { x: width, y: height },
-						})
-					}}
-					onRemove={async () => {
-						await updateScene({ id: scene._id, background: null })
-					}}
-				/>
-
-				{isCurrent ? null : (
-					<Button
-						text="Set as current scene"
-						icon={<LucideCheck />}
-						onClick={async () => {
-							await updateRoom({ id: scene.roomId, currentScene: scene._id })
-							modal?.hide()
-						}}
-						className="w-full"
+		<FormLayout>
+			<FormRow>
+				<FormField label="Scene name" className="flex-1">
+					<EditableInput
+						value={scene.name}
+						onSubmit={(value) => updateScene({ id: scene._id, name: value })}
 					/>
-				)}
-			</FormLayout>
-		</form>
+				</FormField>
+				<FormField label="Cell size" className="w-24">
+					<EditableIntegerInput
+						align="center"
+						value={scene.cellSize}
+						onSubmit={(value) => updateScene({ id: scene._id, cellSize: value })}
+					/>
+				</FormField>
+			</FormRow>
+
+			<ImageUploader
+				imageId={scene.background}
+				onUpload={async (imageId, file) => {
+					const { width, height } = await loadImage(URL.createObjectURL(file))
+					updateScene({
+						id: scene._id,
+						background: imageId,
+						backgroundDimensions: { x: width, y: height },
+					})
+				}}
+				onRemove={async () => {
+					updateScene({ id: scene._id, background: null })
+				}}
+			/>
+
+			{isCurrent ? null : (
+				<Button
+					text="Visit scene"
+					icon={<LucideDoorOpen />}
+					onClick={() => {
+						updateRoom({ id: scene.roomId, currentScene: scene._id })
+					}}
+					className="w-full"
+				/>
+			)}
+		</FormLayout>
 	)
+}
+
+function NewSceneButton(props: React.HTMLAttributes<HTMLButtonElement>) {
+	const createScene = useMutation(api.scenes.functions.create)
+	const convex = useConvex()
+	const room = useRoom()
+	const [open, setOpen] = useState(false)
+
+	const [state, action] = useSafeAction(async () => {
+		const id = await createScene({ name: "New Scene", roomId: room._id })
+		const scene = await convex.query(api.scenes.functions.get, { id })
+		setOpen(true)
+		return { scene }
+	})
+
+	const button = <button onClick={() => action()} {...props} />
+
+	return state.value?.scene ?
+			<SceneEditorModal open={open} setOpen={setOpen} scene={state.value.scene}>
+				{button}
+			</SceneEditorModal>
+		:	button
 }
