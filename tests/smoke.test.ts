daef9227aeb15a88ae720cb5f8234b451edcaf99
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-30 22:48:58 -0500

    almost real tests

--- tests/smoke.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test"
-import { site } from "~/modules/meta/helpers.ts"
-
-test("smoke", async ({ page }) => {
-	await page.goto("/")
-	await expect(page).toHaveTitle(site.title)
-})
