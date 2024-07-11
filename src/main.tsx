f1d063e56909f77c2125a6824026e3ce0e78fe43
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-07-10 20:53:13 -0500

    typed meta env

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
-import { ConvexReactClient } from "convex/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
-import "./index.css";
+import { ConvexAuthProvider } from "@convex-dev/auth/react"
+import { ConvexReactClient } from "convex/react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+import "./index.css"
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ConvexAuthProvider client={convex}>
-      <App />
-    </ConvexAuthProvider>
-  </React.StrictMode>,
-);
+	<React.StrictMode>
+		<ConvexAuthProvider client={convex}>
+			<App />
+		</ConvexAuthProvider>
+	</React.StrictMode>,
+)
