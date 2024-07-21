2aa24daf1d223421dd7eef2c8118856e1f0ec25c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-07-21 04:50:48 -0500

    generic popover

--- src/ui/menu.tsx
+++ src/ui/menu.tsx
@@ -1,116 +1,23 @@
-import { offset, Placement, shift, useFloating } from "@floating-ui/react-dom"
-import React, { useEffect } from "react"
-import { createPortal } from "react-dom"
-import { FocusOn } from "react-focus-on"
-import { useMergedRefs } from "../../lib/react.ts"
+import { ComponentProps } from "react"
 import { Button, ButtonProps } from "./button.tsx"
 import { mergeClassProp } from "./helpers.ts"
-import { Panel } from "./panel.tsx"
-import { fadeZoomTransition } from "./transitions.ts"
+import { Popover, usePopoverContext } from "./popover.tsx"
 
-interface MenuOptions {
-	placement?: Placement
+export { usePopoverContext as useMenuContext }
+
+export function Menu(props: ComponentProps<typeof Popover>) {
+	return <Popover {...props} />
 }
-
-function useMenuState({ placement = "bottom" }: MenuOptions) {
-	const [open, setOpen] = React.useState(false)
-
-	const { refs, floatingStyles } = useFloating({
-		placement,
-		middleware: [offset(8), shift({ padding: 8 })],
-	})
-
-	return {
-		open,
-		setOpen,
-		buttonRef: refs.setReference,
-		panelRef: refs.setFloating,
-		panelStyle: floatingStyles,
-	}
-}
-
-const MenuContext = React.createContext<ReturnType<typeof useMenuState> | null>(
-	null,
-)
-
-export function Menu({
-	children,
-	...props
-}: { children: React.ReactNode } & MenuOptions) {
-	const context = useMenuState(props)
-	return <MenuContext.Provider value={context}>{children}</MenuContext.Provider>
-}
-
-function useMenuContext() {
-	const context = React.useContext(MenuContext)
-	if (context === null) {
-		throw new Error("useMenuContext must be used within a Menu provider")
-	}
-	return context
-}
-
-Menu.Button = MenuButton
-function MenuButton(props: React.ComponentProps<"button">) {
-	const context = useMenuContext()
-	const ref = useMergedRefs(context.buttonRef, props.ref)
-	return (
-		<button
-			type="button"
-			{...props}
-			ref={ref}
-			onClick={(event) => {
-				props.onClick?.(event)
-				context.setOpen((open) => !open)
-			}}
-		/>
-	)
-}
+Menu.Button = Popover.Button
 
 Menu.Panel = MenuPanel
-function MenuPanel(
-	props: React.ComponentProps<"div"> & { children: React.ReactNode },
-) {
-	const context = useMenuContext()
-	const ref = useMergedRefs(context.panelRef, props.ref)
-
-	// FocusOn can't steal focus state when turning on right away,
-	// so we have to delay it on open
-	const [enabled, setEnabled] = React.useState(false)
-	useEffect(() => {
-		setEnabled(context.open)
-	}, [context.open])
-
-	return (
-		<Portal>
-			<div ref={context.panelRef} style={context.panelStyle}>
-				<FocusOn
-					as={Panel}
-					enabled={context.open ? enabled : false}
-					onEscapeKey={() => context.setOpen(false)}
-					onClickOutside={() => context.setOpen(false)}
-					{...mergeClassProp(
-						props,
-						"grid gap-1 p-1",
-						fadeZoomTransition(context.open),
-					)}
-					ref={ref}
-				/>
-			</div>
-		</Portal>
-	)
-}
-
-function Portal({ children }: { children: React.ReactNode }) {
-	const [container, setContainer] = React.useState<HTMLElement | null>(null)
-	React.useEffect(() => {
-		setContainer(document.body)
-	}, [])
-	return container ? createPortal(children, container) : null
+function MenuPanel(props: ComponentProps<typeof Popover.Panel>) {
+	return <Popover.Panel {...mergeClassProp(props, "grid gap-1 p-1")} />
 }
 
 Menu.Item = MenuItem
 function MenuItem(props: ButtonProps) {
-	const context = useMenuContext()
+	const context = usePopoverContext()
 	return (
 		<Button
 			align="start"
---
+++ src/ui/popover.tsx
@@ -0,0 +1,100 @@
+import { offset, Placement, shift, useFloating } from "@floating-ui/react-dom"
+import React, { useEffect } from "react"
+import { FocusOn } from "react-focus-on"
+import { useMergedRefs } from "../../lib/react.ts"
+import { mergeClassProp } from "./helpers.ts"
+import { Panel } from "./panel.tsx"
+import { Portal } from "./portal.tsx"
+import { fadeZoomTransition } from "./transitions.ts"
+
+interface PopoverOptions {
+	placement?: Placement
+}
+
+function usePopoverState({ placement = "bottom" }: PopoverOptions) {
+	const [open, setOpen] = React.useState(false)
+
+	const { refs, floatingStyles } = useFloating({
+		placement,
+		middleware: [offset(8), shift({ padding: 8 })],
+	})
+
+	return {
+		open,
+		setOpen,
+		buttonRef: refs.setReference,
+		panelRef: refs.setFloating,
+		panelStyle: floatingStyles,
+	}
+}
+
+const PopoverContext = React.createContext<ReturnType<
+	typeof usePopoverState
+> | null>(null)
+
+export function Popover({
+	children,
+	...props
+}: { children: React.ReactNode } & PopoverOptions) {
+	const context = usePopoverState(props)
+	return (
+		<PopoverContext.Provider value={context}>
+			{children}
+		</PopoverContext.Provider>
+	)
+}
+
+export function usePopoverContext() {
+	const context = React.useContext(PopoverContext)
+	if (context === null) {
+		throw new Error("usePopoverContext must be used within a Popover provider")
+	}
+	return context
+}
+
+Popover.Button = PopoverButton
+function PopoverButton(props: React.ComponentProps<"button">) {
+	const context = usePopoverContext()
+	const ref = useMergedRefs(context.buttonRef, props.ref)
+	return (
+		<button
+			type="button"
+			{...props}
+			ref={ref}
+			onClick={(event) => {
+				props.onClick?.(event)
+				context.setOpen((open) => !open)
+			}}
+		/>
+	)
+}
+
+Popover.Panel = PopoverPanel
+function PopoverPanel(
+	props: React.ComponentProps<"div"> & { children: React.ReactNode },
+) {
+	const context = usePopoverContext()
+	const ref = useMergedRefs(context.panelRef, props.ref)
+
+	// FocusOn can't steal focus state when turning on right away,
+	// so we have to delay it on open
+	const [enabled, setEnabled] = React.useState(false)
+	useEffect(() => {
+		setEnabled(context.open)
+	}, [context.open])
+
+	return (
+		<Portal>
+			<div ref={context.panelRef} style={context.panelStyle}>
+				<FocusOn
+					as={Panel}
+					enabled={context.open ? enabled : false}
+					onEscapeKey={() => context.setOpen(false)}
+					onClickOutside={() => context.setOpen(false)}
+					{...mergeClassProp(props, fadeZoomTransition(context.open))}
+					ref={ref}
+				/>
+			</div>
+		</Portal>
+	)
+}
