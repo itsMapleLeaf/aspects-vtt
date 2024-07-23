0501cfcc1797063abf27aebdb98a1faa4577fe92
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-07-23 01:05:12 -0500

    add optional state overload

--- app/types.d.ts
+++ app/types.d.ts
@@ -1,2 +1,9 @@
 import {} from "@total-typescript/ts-reset"
 import {} from "vite/client"
+
+import "react"
+declare module "react" {
+	export function useActionState<State, Payload>(
+		action: (state: Awaited<State>, payload: Payload) => State | Promise<State>,
+	): [state: Awaited<State>, dispatch: (payload: Payload) => void, isPending: boolean]
+}
