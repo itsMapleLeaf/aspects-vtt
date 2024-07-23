f2cf1c622edc7aefaa33145f39f24587b1a2a535
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-07-22 23:30:12 -0500

    style tweaks

--- app/ui/EmptyState.tsx
+++ app/ui/EmptyState.tsx
@@ -39,14 +39,14 @@ export function EmptyState({
 	return (
 		<section
 			className={twMerge(
-				"flex-center-col mx-auto w-full max-w-screen-sm gap-4 text-center",
+				"flex-center-col mx-auto w-full max-w-screen-sm gap-2 text-center",
 				className,
 			)}
 		>
-			<div aria-hidden className="text-primary-500 opacity-60 *:size-16">
+			<div aria-hidden className="opacity-40 *:size-16 empty:hidden">
 				{icon}
 			</div>
-			<p className="text-xl font-light">{message}</p>
+			<p className="text-xl font-light opacity-75">{message}</p>
 			<div className="flex-center-row flex-wrap gap-2 empty:hidden">{actions}</div>
 			{children}
 		</section>
