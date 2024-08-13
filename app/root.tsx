0abddf23e95e49ddc8c593017dd9129e017822dd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-08-12 20:54:48 -0500

    weird change, but this should help some things later

--- app/root.tsx
+++ app/root.tsx
@@ -6,15 +6,22 @@ import "./root.css"
 
 import { ConvexAuthProvider } from "@convex-dev/auth/react"
 import type { MetaFunction } from "@remix-run/node"
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react"
 import { ConvexReactClient } from "convex/react"
 import { Suspense, useState } from "react"
 import { clientEnv } from "./env.ts"
+import { raise } from "./helpers/errors.ts"
 import { getSiteMeta } from "./modules/meta/helpers.ts"
 import { PromptProvider } from "./ui/Prompt.tsx"
 import { Toaster } from "./ui/Toaster.tsx"
 
 export const meta: MetaFunction = () => getSiteMeta()
+
+export const loader = () => {
+	return {
+		convexUrl: process.env.CONVEX_URL || process.env.VITE_CONVEX_URL || raise("CONVEX_URL not set"),
+	}
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
@@ -41,7 +48,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-	const [convex] = useState(() => new ConvexReactClient(clientEnv.VITE_CONVEX_URL))
+	const { convexUrl } = useLoaderData<typeof loader>()
+	const [convex] = useState(() => new ConvexReactClient(convexUrl))
 	return (
 		<ConvexAuthProvider client={convex}>
 			<Outlet />
