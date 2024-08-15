e4a0a0eec216a0573778e35099fb5c651dbb0296
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-08-15 03:27:37 -0500

    CheckboxField: add optional description

--- app/ui/CheckboxField.tsx
+++ app/ui/CheckboxField.tsx
@@ -1,38 +1,48 @@
 import * as Lucide from "lucide-react"
 import { useId } from "react"
-import { FormField } from "./Form.tsx"
+import {
+	FormFieldDescription,
+	FormFieldLabel,
+	FormFieldProvider,
+} from "./Form.tsx"
 
 export function CheckboxField({
 	label,
+	description,
 	checked,
 	onChange,
 }: {
 	label: string
+	description?: string
 	checked: boolean
 	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }) {
 	const id = useId()
 	return (
-		<FormField
-			label={label}
-			htmlFor={id}
-			className="flex-row-reverse justify-end gap-1.5"
-		>
-			<div className="relative flex size-5 items-center justify-center overflow-clip">
-				<input
-					className="peer size-full appearance-none rounded border-2 border-primary-400 bg-primary-100 transition checked:bg-primary-300/50 hover:bg-primary-200 checked:hover:bg-primary-300 active:border-primary-600 active:duration-0"
-					id={id}
-					aria-label={label}
-					type="checkbox"
-					checked={checked}
-					onChange={onChange}
-				/>
-				<Lucide.X
-					className="pointer-events-none invisible absolute size-5 text-primary-700 peer-checked:visible"
-					strokeWidth={3}
-					absoluteStrokeWidth
-				/>
+		<FormFieldProvider htmlFor={id}>
+			<div className="flex items-center gap-1.5">
+				<div className="relative flex size-5 items-center justify-center overflow-clip">
+					<input
+						className="peer size-full appearance-none rounded border-2 border-primary-400 bg-primary-100 transition checked:bg-primary-300/50 hover:bg-primary-200 checked:hover:bg-primary-300 active:border-primary-600 active:duration-0"
+						id={id}
+						aria-label={label}
+						type="checkbox"
+						checked={checked}
+						onChange={onChange}
+					/>
+					<Lucide.X
+						className="pointer-events-none invisible absolute size-5 text-primary-700 peer-checked:visible"
+						strokeWidth={3}
+						absoluteStrokeWidth
+					/>
+				</div>
+				<div className="flex flex-col">
+					<FormFieldLabel>{label}</FormFieldLabel>
+					{description && (
+						<FormFieldDescription>{description}</FormFieldDescription>
+					)}
+				</div>
 			</div>
-		</FormField>
+		</FormFieldProvider>
 	)
 }
