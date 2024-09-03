9b4729e6e694b644129d3a38d19fc10514930bcf
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-02 19:51:14 -0500

    explicit default modal width

--- src/ui/modal.tsx
+++ src/ui/modal.tsx
@@ -25,7 +25,7 @@ export function ModalPanel({
 			unmountOnHide
 			{...props}
 			className={panel(
-				"fixed inset-auto left-1/2 top-1/2 flex max-h-[calc(100vh-2rem)] max-w-[calc(100vw-2rem)] -translate-x-1/2 -translate-y-1/2 scale-95 flex-col rounded-lg bg-primary-800 p-3 opacity-0 transition gap-3 data-[enter]:scale-100 data-[enter]:opacity-100",
+				"fixed inset-auto left-1/2 top-1/2 flex max-h-[calc(100vh-2rem)] w-full max-w-[calc(min(32rem,100vw-2rem))] -translate-x-1/2 -translate-y-1/2 scale-95 flex-col rounded-lg bg-primary-800 p-3 opacity-0 transition gap-3 data-[enter]:scale-100 data-[enter]:opacity-100",
 				props.className,
 			)}
 		>
