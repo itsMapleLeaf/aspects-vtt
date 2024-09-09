3067dd50667b6e4e81b20269fde490a56d1bd8cd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-09 12:07:17 -0500

    add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		tsconfigPaths(),
 		remix({
 			appDirectory: "src",
 			future: {
