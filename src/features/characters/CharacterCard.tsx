5074030c6b1e204dbb83c477286c80df3dbc6710
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-21 00:12:52 -0500

    use avatar in CharacterCard

--- src/features/characters/CharacterCard.tsx
+++ src/features/characters/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import * as Lucide from "lucide-react"
+import { Avatar } from "~/components/Avatar.tsx"
 import { Button } from "~/components/Button.tsx"
 import { Heading } from "~/components/Heading.tsx"
 import { secondaryHeading } from "~/styles/text.ts"
@@ -14,17 +15,7 @@ export function CharacterCard({ character }: { character: ApiCharacter }) {
 	return (
 		<div className={"flex flex-col gap-3"}>
 			<div className="flex items-center gap">
-				<div className="size-14 overflow-clip rounded-full border border-primary-600">
-					{character.imageUrl ?
-						<img
-							src={character.imageUrl}
-							className="size-full rounded-full object-cover object-top p-px"
-						/>
-					:	<div className="size-full bg-primary-900">
-							<Lucide.VenetianMask className="size-full scale-[0.6]" />
-						</div>
-					}
-				</div>
+				<Avatar src={character.imageUrl} className="size-14" />
 				<div>
 					<Heading className={secondaryHeading("leading-none")}>
 						{character.name}
