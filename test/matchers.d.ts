2b3f587fd6aec30bcb5cdf3eb089cc3c364bea33
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-25 16:04:33 -0500

    set up dom testing

---
+++ test/matchers.d.ts
@@ -0,0 +1,8 @@
+import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
+import "bun:test"
+
+declare module "bun:test" {
+	interface Matchers<T>
+		extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
+	interface AsymmetricMatchers extends TestingLibraryMatchers {}
+}
