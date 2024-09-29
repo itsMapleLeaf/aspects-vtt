a9a3eb9d1a925f3f070dfc92ada27b8b8421d239
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-29 18:33:39 -0500

    add ensure functions

--- shared/errors.ts
+++ shared/errors.ts
@@ -1,6 +1,20 @@
-export function raise(error: string | object): never {
-	if (typeof error === "string") {
-		throw new Error(error)
+export function raise(input: string | object): never {
+	if (typeof input === "string") {
+		const error = new Error(input)
+		Error.captureStackTrace(error, raise)
+		throw error
 	}
-	throw error
+	throw input
+}
+
+export function ensure<Input, Asserted extends Input>(
+	input: Input,
+	condition: (input: Input) => input is Asserted,
+	error?: string | object,
+): Asserted {
+	return condition(input) ? input : raise(error ?? "Assertion failed")
+}
+
+export function ensureSomething<T>(input: T, error?: string | object) {
+	return ensure(input, (input) => input != null, error)
 }
