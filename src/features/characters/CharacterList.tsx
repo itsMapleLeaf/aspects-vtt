cbc836e0ba4976889deedd0af696d6d15a68dfec
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-09-29 18:34:40 -0500

    searchable characters

--- src/features/characters/CharacterList.tsx
+++ src/features/characters/CharacterList.tsx
@@ -1,6 +1,10 @@
 import { useQuery } from "convex/react"
+import { LucideUserX2 } from "lucide-react"
+import { matchSorter } from "match-sorter"
+import { useState } from "react"
 import { LoadingIcon } from "~/components/LoadingIcon.tsx"
 import { api } from "~/convex/_generated/api.js"
+import { SearchListLayout } from "../inventory/SearchListLayout.tsx"
 import { useRoomContext } from "../rooms/context.tsx"
 import { CharacterCard } from "./CharacterCard.tsx"
 
@@ -9,15 +13,29 @@ export function CharacterList() {
 	const characters = useQuery(api.characters.list, {
 		roomId: room._id,
 	})
-	return characters === undefined ? (
-		<div className="flex flex-col items-center py-8">
-			<LoadingIcon />
-		</div>
-	) : (
-		<div className="flex h-full min-h-0 flex-col overflow-y-auto border-t border-primary-700 *:border-b *:border-primary-700 *:py-3">
-			{characters.map((character) => (
-				<CharacterCard key={character.public._id} character={character} />
-			))}
-		</div>
+	const [search, setSearch] = useState("")
+
+	if (characters === undefined) {
+		return (
+			<div className="flex flex-col items-center py-8">
+				<LoadingIcon />
+			</div>
+		)
+	}
+
+	const filteredCharacters = matchSorter(characters, search, {
+		keys: ["identity.name", "public.race"],
+	})
+
+	return (
+		<SearchListLayout
+			items={filteredCharacters}
+			itemKey={(character) => character.public._id}
+			renderItem={(character) => <CharacterCard character={character} />}
+			onSearch={setSearch}
+			emptyStateIcon={<LucideUserX2 />}
+			emptyStateText="No characters found"
+			className="h-full border-t border-primary-700"
+		/>
 	)
 }
