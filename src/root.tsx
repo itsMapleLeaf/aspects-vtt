68a916cf56ee586bfc27d76954bcdaac34b52687
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-10-23 23:27:01 -0500

    cute favicon :)

--- src/root.tsx
+++ src/root.tsx
@@ -56,6 +56,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" href="/favicon.svg" />
 				<Meta />
 				<Links />
 				<title>Aspects VTT</title>
---
+++ src/routes/favicon[.]svg.tsx
@@ -0,0 +1,26 @@
+import {
+	LucideDroplet,
+	LucideFlame,
+	LucideMoon,
+	LucideSun,
+	LucideTornado,
+} from "lucide-react"
+import { renderToStaticMarkup } from "react-dom/server"
+import { ensure } from "../../shared/errors.ts"
+
+const icons = [LucideFlame, LucideDroplet, LucideTornado, LucideSun, LucideMoon]
+
+export function loader() {
+	const Icon = ensure(icons[Math.floor(Math.random() * icons.length)])
+	return new Response(
+		renderToStaticMarkup(
+			<Icon stroke="#8a96bc" fill="#141732" width="100%" height="100%" />,
+		),
+		{
+			headers: {
+				"Content-Type": "image/svg+xml",
+				"Cache-Control": "no-store",
+			},
+		},
+	)
+}
