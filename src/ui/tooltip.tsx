1f657655b51d4f8b0c3a6999e2d684fc09154995
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-10-25 00:08:32 -0500

    tooltip style tweaks

--- src/ui/tooltip.tsx
+++ src/ui/tooltip.tsx
@@ -2,6 +2,7 @@ import * as Ariakit from "@ariakit/react"
 
 import { cn } from "~/ui/helpers"
 
+// todo: rename this and export a simple `Tooltip` that doesn't require a tree of bullshit
 const Tooltip = Ariakit.TooltipProvider
 
 const TooltipTrigger = Ariakit.TooltipAnchor
@@ -10,10 +11,11 @@ const TooltipContent = ({ className, ...props }: Ariakit.TooltipProps) => (
 	<Ariakit.Tooltip
 		gutter={8}
 		className={cn(
-			"scale-90 overflow-hidden rounded-md bg-primary-100 px-2 py-1.5 text-sm leading-none text-primary-900 opacity-0 shadow transition data-[enter]:scale-100 data-[enter]:opacity-100",
+			"max-w-48 scale-90 overflow-hidden rounded-md bg-primary-100 px-2 py-1.5 text-center text-sm leading-tight text-primary-900 opacity-0 shadow transition data-[enter]:scale-100 data-[enter]:opacity-100",
 			className,
 		)}
 		unmountOnHide
+		portal
 		{...props}
 	/>
 )
