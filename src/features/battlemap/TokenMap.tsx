dd1f2d09df5b618c590499d45e973fd5f80b7e7e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-10-27 04:53:55 -0500

    undo fun but cringe ideas

--- src/features/battlemap/TokenMap.tsx
+++ src/features/battlemap/TokenMap.tsx
@@ -1,64 +1,15 @@
 import { useQuery } from "convex/react"
 import { clamp } from "lodash-es"
-import { atom, computed } from "nanostores"
-import { ReactNode, useEffect, useState } from "react"
-import { useLatestRef } from "~/common/react/core.ts"
+import { ReactNode, useRef, useState } from "react"
+import { useEffectEvent } from "~/common/react/core.ts"
 import { api } from "~/convex/_generated/api.js"
 import { Id } from "~/convex/_generated/dataModel.js"
+import { useEventListener } from "~/lib/react.ts"
 import { Vec } from "~/shared/vec.ts"
 import { ApiCharacter } from "../characters/types.ts"
 import { getImageUrl } from "../images/getImageUrl.ts"
 import { ApiScene } from "../scenes/types.ts"
 import { ApiToken } from "./types.ts"
-
-export function TokenMap({ scene }: { scene: ApiScene }) {
-	const tokens = useQuery(api.tokens.list, { sceneId: scene._id }) ?? []
-	const [selectedTokenIds, setSelectedTokenIds] = useState(
-		new Set<Id<"characterTokens">>(),
-	)
-
-	return (
-		<TokenMapViewport
-			onSelection={(rect) => {
-				setSelectedTokenIds(
-					new Set(
-						tokens
-							.filter((it) => {
-								const center = Vec.from(it.position).plus(scene.cellSize / 2)
-								return (
-									center.x >= rect.x &&
-									center.x < rect.x + rect.width &&
-									center.y >= rect.y &&
-									center.y < rect.y + rect.height
-								)
-							})
-							.map((it) => it._id),
-					),
-				)
-			}}
-		>
-			{scene.battlemapBackgroundId && (
-				<img
-					src={getImageUrl(scene.battlemapBackgroundId)}
-					alt=""
-					className="max-w-none"
-					draggable={false}
-				/>
-			)}
-			{tokens.map((token) =>
-				token.characterId ? (
-					<CharacterTokenElement
-						key={token._id}
-						token={token}
-						character={token.character}
-						scene={scene}
-						selected={selectedTokenIds.has(token._id)}
-					/>
-				) : null,
-			)}
-		</TokenMapViewport>
-	)
-}
 
 /** Corresponds to the `button` property on pointer events (**not** `buttons`) */
 type PointerButton = (typeof PointerButton)[keyof typeof PointerButton]
@@ -68,313 +19,263 @@ const PointerButton = {
 	right: 2,
 } as const
 
-function TokenMapViewport({
-	children,
-	onSelection,
-}: {
-	children: ReactNode
-	onSelection: (rect: {
-		x: number
-		y: number
-		width: number
-		height: number
-	}) => void
-}) {
-	const onSelectRef = useLatestRef(onSelection)
+export function TokenMap({ scene }: { scene: ApiScene }) {
+	const tokens = useQuery(api.tokens.list, { sceneId: scene._id }) ?? []
+	const [selectedTokenIds, setSelectedTokenIds] = useState(
+		new Set<Id<"characterTokens">>(),
+	)
 
-	const [controller] = useState(function createTokenMapController() {
-		const pointerButtonLeft = atom<"up" | "down" | "dragging">("up")
-		const pointerButtonRight = atom<"up" | "down" | "dragging">("up")
+	const [pointerState, setPointerState] = useState({
+		buttonLeft: "up" as "up" | "down" | "dragging",
+		buttonRight: "up" as "up" | "down" | "dragging",
+		dragStart: Vec.from(0),
+		dragEnd: Vec.from(0),
+		viewportOffset: Vec.from(0),
+		viewportZoom: 0,
+		selectionStart: Vec.from(0),
+		selectionEnd: Vec.from(0),
+	})
 
-		const viewportDragStart = atom(Vec.from(0))
-		const viewportDragEnd = atom(Vec.from(0))
+	const shouldPreventContextMenu = useRef(false)
 
-		const viewportOffsetBase = atom(Vec.from(0))
-		const viewportZoom = atom(0)
+	const viewportScaleCoefficient = 1.3
+	const viewportScale = viewportScaleCoefficient ** pointerState.viewportZoom
 
-		const selectionStart = atom(Vec.from(0))
-		const selectionEnd = atom(Vec.from(0))
+	const draggingOffset =
+		pointerState.buttonRight === "dragging"
+			? pointerState.dragEnd.minus(pointerState.dragStart)
+			: Vec.from(0)
 
-		let shouldPreventContextMenu = false
+	const viewportOffset = pointerState.viewportOffset.plus(draggingOffset)
 
-		const draggingOffset = computed(
-			[pointerButtonRight, viewportDragStart, viewportDragEnd],
-			(pointerButtonRight, pointerStart, pointerEnd) =>
-				pointerButtonRight === "dragging"
-					? pointerEnd.minus(pointerStart)
-					: Vec.from(0),
-		)
+	const selectionTopLeft = Vec.from([
+		Math.min(pointerState.selectionStart.x, pointerState.selectionEnd.x),
+		Math.min(pointerState.selectionStart.y, pointerState.selectionEnd.y),
+	])
 
-		const viewportOffset = computed(
-			[viewportOffsetBase, draggingOffset],
-			(viewportOffsetBase, draggingOffset) =>
-				viewportOffsetBase.plus(draggingOffset),
-		)
+	const selectionBottomRight = Vec.from([
+		Math.max(pointerState.selectionStart.x, pointerState.selectionEnd.x),
+		Math.max(pointerState.selectionStart.y, pointerState.selectionEnd.y),
+	])
 
-		const viewportScaleCoefficient = 1.3
-		const viewportScale = computed(
-			[viewportZoom],
-			(viewportZoom) => viewportScaleCoefficient ** viewportZoom,
-		)
+	const selectionSize = Vec.from([
+		selectionBottomRight.x - selectionTopLeft.x,
+		selectionBottomRight.y - selectionTopLeft.y,
+	])
 
-		const viewportTransform = computed(
-			[viewportOffset, viewportScale],
-			(offset, scale) => `translate(${offset.toCSSPixels()}) scale(${scale})`,
-		)
-
-		const selectionTopLeft = computed(
-			[selectionStart, selectionEnd],
-			(selectionStart, selectionEnd) =>
-				Vec.from([
-					Math.min(selectionStart.x, selectionEnd.x),
-					Math.min(selectionStart.y, selectionEnd.y),
-				]),
-		)
-
-		const selectionBottomRight = computed(
-			[selectionStart, selectionEnd],
-			(selectionStart, selectionEnd) =>
-				Vec.from([
-					Math.max(selectionStart.x, selectionEnd.x),
-					Math.max(selectionStart.y, selectionEnd.y),
-				]),
-		)
-
-		const selectionSize = computed(
-			[selectionTopLeft, selectionBottomRight],
-			(selectionTopLeft, selectionBottomRight) =>
-				Vec.from([
-					selectionBottomRight.x - selectionTopLeft.x,
-					selectionBottomRight.y - selectionTopLeft.y,
-				]),
-		)
-
-		const selectionStyle = computed(
-			[selectionTopLeft, selectionSize],
-			(selectionTopLeft, selectionSize) => ({
-				width: `${selectionSize.x}px`,
-				height: `${selectionSize.y}px`,
-				transform: `translate(${selectionTopLeft.toCSSPixels()})`,
-				opacity: pointerButtonLeft.get() === "dragging" ? "1" : "0",
-				transitionDuration:
-					pointerButtonLeft.get() === "dragging" ? "0" : undefined,
-			}),
-		)
-
-		function bindRootListeners(element: HTMLElement) {
-			const controller = new AbortController()
-
-			element.addEventListener(
-				"pointerdown",
-				(event) => {
-					if (event.button === PointerButton.left) {
-						event.preventDefault()
-						pointerButtonLeft.set("down")
-						selectionStart.set(Vec.from(event))
-						selectionEnd.set(Vec.from(event))
-					}
-					if (event.button === PointerButton.right) {
-						event.preventDefault()
-						pointerButtonRight.set("down")
-						viewportDragStart.set(Vec.from(event))
-						viewportDragEnd.set(Vec.from(event))
-					}
-				},
-				{ signal: controller.signal },
+	const onSelection = useEffectEvent(
+		(rect: { x: number; y: number; width: number; height: number }): void => {
+			setSelectedTokenIds(
+				new Set(
+					tokens
+						.filter((it) => {
+							const center = Vec.from(it.position).plus(scene.cellSize / 2)
+							return (
+								center.x >= rect.x &&
+								center.x < rect.x + rect.width &&
+								center.y >= rect.y &&
+								center.y < rect.y + rect.height
+							)
+						})
+						.map((it) => it._id),
+				),
 			)
+		},
+	)
 
-			element.addEventListener(
-				"wheel",
-				(event) => {
-					if (event.deltaY === 0) return
-
-					const delta = Math.round(event.deltaY / 100) * -1
-
-					const currentZoom = viewportZoom.get()
-					const currentOffset = viewportOffset.get()
-
-					const pointerOffset = Vec.from(event).minus(currentOffset)
-					const nextViewportZoom = clamp(currentZoom + delta, -10, 10)
-
-					const currentViewportScale = viewportScaleCoefficient ** currentZoom
-					const nextViewportScale = viewportScaleCoefficient ** nextViewportZoom
-					const scaleRatio = nextViewportScale / currentViewportScale
-
-					viewportOffsetBase.set(
-						viewportOffsetBase.get().plus(pointerOffset.times(1 - scaleRatio)),
-					)
-					viewportZoom.set(nextViewportZoom)
-				},
-				{ signal: controller.signal, passive: false },
-			)
-
-			return () => {
-				controller.abort()
+	useEventListener(window, "pointermove", (event) => {
+		setPointerState((state) => {
+			if (state.buttonLeft === "down" || state.buttonLeft === "dragging") {
+				return {
+					...state,
+					buttonLeft: "dragging",
+					selectionEnd: Vec.from(event),
+				}
 			}
+			if (state.buttonRight === "down" || state.buttonRight === "dragging") {
+				return {
+					...state,
+					buttonRight: "dragging",
+					dragEnd: Vec.from(event),
+				}
+			}
+			return state
+		})
+		shouldPreventContextMenu.current = false
+	})
+
+	useEventListener(window, "pointerup", (event) => {
+		if (
+			event.button === PointerButton.left &&
+			(pointerState.buttonLeft === "down" ||
+				pointerState.buttonLeft === "dragging")
+		) {
+			setPointerState((state) => ({
+				...state,
+				buttonLeft: "up",
+				selectionEnd: Vec.from(event),
+			}))
+
+			const width = selectionSize.x / viewportScale
+			const height = selectionSize.y / viewportScale
+
+			onSelection({
+				...selectionTopLeft
+					.minus(viewportOffset)
+					.dividedBy(viewportScale)
+					.toJSON(),
+				width,
+				height,
+			})
 		}
 
-		function bindWindowListeners() {
-			const controller = new AbortController()
-
-			window.addEventListener(
-				"pointermove",
-				(event) => {
-					if (
-						pointerButtonLeft.get() === "down" ||
-						pointerButtonLeft.get() === "dragging"
-					) {
-						pointerButtonLeft.set("dragging")
-						selectionEnd.set(Vec.from(event))
-					}
-
-					if (
-						pointerButtonRight.get() === "down" ||
-						pointerButtonRight.get() === "dragging"
-					) {
-						pointerButtonRight.set("dragging")
-						viewportDragEnd.set(Vec.from(event))
-					}
-
-					shouldPreventContextMenu = false
-				},
-				{ signal: controller.signal },
-			)
-
-			window.addEventListener(
-				"pointerup",
-				(event) => {
-					if (
-						event.button === PointerButton.left &&
-						(pointerButtonLeft.get() === "down" ||
-							pointerButtonLeft.get() === "dragging")
-					) {
-						pointerButtonLeft.set("up")
-						selectionEnd.set(Vec.from(event))
-
-						const { x: width, y: height } = selectionSize
-							.get()
-							.dividedBy(viewportScale.get())
-
-						const rect = {
-							...selectionTopLeft
-								.get()
-								.minus(viewportOffset.get())
-								.dividedBy(viewportScale.get())
-								.toJSON(),
-							width,
-							height,
-						}
-						onSelectRef.current(rect)
-					}
-
-					if (
-						event.button === PointerButton.right &&
-						pointerButtonRight.get() === "down"
-					) {
-						pointerButtonRight.set("up")
-						shouldPreventContextMenu = true
-					}
-
-					if (
-						event.button === PointerButton.right &&
-						pointerButtonRight.get() === "dragging"
-					) {
-						pointerButtonRight.set("up")
-						viewportOffsetBase.set(
-							viewportOffsetBase
-								.get()
-								.plus(viewportDragEnd.get().minus(viewportDragStart.get())),
-						)
-						shouldPreventContextMenu = true
-					}
-				},
-				{ signal: controller.signal },
-			)
-
-			window.addEventListener(
-				"blur",
-				(event) => {
-					if (pointerButtonRight.get() === "down") {
-						event.preventDefault()
-						pointerButtonRight.set("up")
-						viewportOffsetBase.set(
-							viewportOffsetBase
-								.get()
-								.plus(viewportDragEnd.get().minus(viewportDragStart.get())),
-						)
-					}
-					if (pointerButtonRight.get() === "dragging") {
-						event.preventDefault()
-						pointerButtonRight.set("up")
-						viewportOffsetBase.set(
-							viewportOffsetBase
-								.get()
-								.plus(viewportDragEnd.get().minus(viewportDragStart.get())),
-						)
-					}
-				},
-				{ signal: controller.signal },
-			)
-
-			window.addEventListener(
-				"contextmenu",
-				(event) => {
-					if (shouldPreventContextMenu) {
-						event.preventDefault()
-						shouldPreventContextMenu = false
-					}
-				},
-				{ signal: controller.signal },
-			)
-
-			return () => {
-				controller.abort()
-			}
+		if (
+			event.button === PointerButton.right &&
+			pointerState.buttonRight === "down"
+		) {
+			setPointerState((state) => ({ ...state, buttonRight: "up" }))
+			shouldPreventContextMenu.current = true
 		}
 
-		return {
-			bindRootListeners,
-			bindWindowListeners,
-			viewportTransform,
-			selectionStyle,
+		if (
+			event.button === PointerButton.right &&
+			pointerState.buttonRight === "dragging"
+		) {
+			setPointerState((state) => ({
+				...state,
+				buttonRight: "up",
+				viewportOffset: state.viewportOffset.plus(
+					state.dragEnd.minus(state.dragStart),
+				),
+			}))
+			shouldPreventContextMenu.current = true
 		}
 	})
 
-	useEffect(() => controller.bindWindowListeners(), [controller])
+	useEventListener(window, "blur", () => {
+		if (
+			pointerState.buttonRight === "down" ||
+			pointerState.buttonRight === "dragging"
+		) {
+			setPointerState((state) => ({
+				...state,
+				buttonRight: "up",
+				viewportOffset: state.viewportOffset.plus(
+					state.dragEnd.minus(state.dragStart),
+				),
+			}))
+		}
+	})
+
+	useEventListener(window, "contextmenu", (event) => {
+		if (shouldPreventContextMenu.current) {
+			event.preventDefault()
+			shouldPreventContextMenu.current = false
+		}
+	})
+
+	const handleRootPointerDown = (event: React.PointerEvent) => {
+		if (event.button === PointerButton.right) {
+			event.preventDefault()
+			setPointerState((state) => ({
+				...state,
+				buttonRight: "down",
+				dragStart: Vec.from(event.nativeEvent),
+				dragEnd: Vec.from(event.nativeEvent),
+			}))
+		}
+	}
+
+	const handleSelectionInputPointerDown = (event: React.PointerEvent) => {
+		if (event.button === PointerButton.left) {
+			event.preventDefault()
+			setPointerState((state) => ({
+				...state,
+				buttonLeft: "down",
+				selectionStart: Vec.from(event.nativeEvent),
+				selectionEnd: Vec.from(event.nativeEvent),
+			}))
+		}
+	}
+
+	const handleWheel = (event: React.WheelEvent) => {
+		if (event.deltaY === 0) return
+
+		const delta = Math.round(event.deltaY / 100) * -1
+		const pointerOffset = Vec.from(event.nativeEvent).minus(viewportOffset)
+		const nextViewportZoom = clamp(pointerState.viewportZoom + delta, -10, 10)
+
+		const currentViewportScale =
+			viewportScaleCoefficient ** pointerState.viewportZoom
+		const nextViewportScale = viewportScaleCoefficient ** nextViewportZoom
+		const scaleRatio = nextViewportScale / currentViewportScale
+
+		setPointerState((state) => ({
+			...state,
+			viewportZoom: nextViewportZoom,
+			viewportOffset: state.viewportOffset.plus(
+				pointerOffset.times(1 - scaleRatio),
+			),
+		}))
+	}
 
 	return (
 		<div
 			className="absolute inset-0 overflow-clip"
-			ref={(element) => {
-				if (element) return controller.bindRootListeners(element)
-			}}
+			onPointerDown={handleRootPointerDown}
+			onWheel={handleWheel}
 		>
 			<div
-				className="absolute inset-0 origin-top-left"
-				ref={(element) => {
-					if (!element) return
-					return controller.viewportTransform.subscribe((transform) => {
-						element.style.transform = transform
-					})
+				className="pointer-events-none absolute inset-0 origin-top-left"
+				style={{
+					transform: `translate(${viewportOffset.toCSSPixels()}) scale(${viewportScale})`,
 				}}
 			>
-				{children}
+				{scene.battlemapBackgroundId && (
+					<img
+						src={getImageUrl(scene.battlemapBackgroundId)}
+						alt=""
+						className="max-w-none"
+						draggable={false}
+					/>
+				)}
 			</div>
+
 			<div
-				className="rounded-sm border-2 border-accent-900 bg-accent-600/50 transition-opacity"
-				ref={(element) => {
-					if (!element) return
-					return controller.selectionStyle.subscribe(
-						({ width, height, transform, opacity, transitionDuration }) => {
-							element.style.width = width
-							element.style.height = height
-							element.style.transform = transform
-							element.style.opacity = opacity
-							element.style.transitionDuration = transitionDuration ?? ""
-						},
-					)
-				}}
+				className="absolute inset-0"
+				onPointerDown={handleSelectionInputPointerDown}
 			></div>
+
+			<div
+				className="pointer-events-children absolute inset-0 origin-top-left"
+				style={{
+					transform: `translate(${viewportOffset.toCSSPixels()}) scale(${viewportScale})`,
+				}}
+			>
+				{tokens.map((token) =>
+					token.characterId ? (
+						<CharacterTokenElement
+							key={token._id}
+							token={token}
+							character={token.character}
+							scene={scene}
+							selected={selectedTokenIds.has(token._id)}
+						/>
+					) : null,
+				)}
+			</div>
+
+			<div
+				className="pointer-events-none rounded-sm border-2 border-accent-900 bg-accent-600/50 transition-opacity"
+				style={{
+					width: `${selectionSize.x}px`,
+					height: `${selectionSize.y}px`,
+					transform: `translate(${selectionTopLeft.toCSSPixels()})`,
+					opacity: pointerState.buttonLeft === "dragging" ? 1 : 0,
+					transitionDuration:
+						pointerState.buttonLeft === "dragging" ? "0s" : undefined,
+				}}
+			/>
 		</div>
 	)
 }
