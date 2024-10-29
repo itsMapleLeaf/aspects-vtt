096c8359cf3fbdb1bf2b2f64443fdc6f430d832f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-10-28 22:21:43 -0500

    fix activity token font size

--- src/features/battlemap/ActivityTokenElement.tsx
+++ src/features/battlemap/ActivityTokenElement.tsx
@@ -1,5 +1,4 @@
 import { ComponentProps } from "react"
-import { twMerge } from "tailwind-merge"
 import { BaseTokenElement } from "./BaseTokenElement.tsx"
 
 export function ActivityTokenElement({
@@ -7,18 +6,17 @@ export function ActivityTokenElement({
 	...props
 }: { selected: boolean } & ComponentProps<typeof BaseTokenElement>) {
 	return (
-		<BaseTokenElement
-			{...props}
-			className={twMerge("@container", props.className)}
-		>
-			<div className="absolute inset-[15%] rounded-full bg-white/50 duration-500 ease-in animate-in zoom-in-110 direction-alternate repeat-infinite" />
-			<div className="absolute inset-[20%] rounded-full bg-white/75" />
-			<div className="absolute inset-0 grid place-items-center text-[6cqh] font-bold text-gray-900">
-				!
+		<BaseTokenElement {...props}>
+			<div className="absolute inset-0 size-full @container">
+				<div className="absolute inset-[15%] rounded-full bg-white/50 duration-500 ease-in animate-in zoom-in-110 direction-alternate repeat-infinite" />
+				<div className="absolute inset-[20%] rounded-full bg-white/75" />
+				<div className="absolute inset-0 grid place-items-center text-[50cqw] font-bold text-gray-900">
+					!
+				</div>
+				{selected && (
+					<div className="pointer-events-none absolute inset-[20%] rounded-full border-2 border-accent-900 bg-accent-600/50 transition-opacity" />
+				)}
 			</div>
-			{selected && (
-				<div className="pointer-events-none absolute inset-[20%] rounded-full border-2 border-accent-900 bg-accent-600/50 transition-opacity" />
-			)}
 		</BaseTokenElement>
 	)
 }
