8a6b1fb546040c64b4a37dfafd67f9e71a5a51fd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-11-04 00:06:51 -0600

    add useDictionary

--- lib/react/state.ts
+++ lib/react/state.ts
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { useLatestRef } from "../react/core.ts"
 
 export function useSwitch(initialOn: boolean) {
@@ -44,5 +44,73 @@ export function useDebouncedCallback<Args extends unknown[]>(
 			clearTimeout(timeoutRef.current)
 		}
 		return debounced
-	}, [period])
+	}, [period, callbackRef])
+}
+
+export function useDictionary<K extends PropertyKey, V>(
+	initial?: Record<K, V>,
+) {
+	const [items, setItems] = useState<{ [_ in K]?: V }>(initial ?? {})
+
+	const get = (key: K): V | undefined => items[key]
+
+	const set = (key: K, value: V): V => {
+		setItems((prevItems) => ({ ...prevItems, [key]: value }))
+		return value
+	}
+
+	const remove = (key: K) => {
+		setItems((prevItems) => {
+			const { [key]: _, ...rest } = prevItems
+			return rest as Record<K, V>
+		})
+	}
+
+	const update = (key: K, next: (current: V) => V): V | undefined => {
+		const currentValue = items[key]
+		if (currentValue !== undefined) {
+			const newValue = next(currentValue)
+			setItems((prevItems) => ({ ...prevItems, [key]: newValue }))
+			return newValue
+		}
+		return undefined
+	}
+
+	const patch = (key: K, patch: Partial<V>): V | undefined => {
+		const currentValue = items[key]
+		if (currentValue !== undefined) {
+			const newValue = { ...currentValue, ...patch }
+			setItems((prevItems) => ({ ...prevItems, [key]: newValue }))
+			return newValue as V
+		}
+		return undefined
+	}
+
+	const keys = (): K[] => {
+		return Object.keys(items) as K[]
+	}
+
+	const values = (): V[] => {
+		return Object.values(items)
+	}
+
+	const entries = (): readonly [K, V][] => {
+		return Object.entries(items) as [K, V][]
+	}
+
+	const size = (): number => {
+		return Object.keys(items).length
+	}
+
+	return {
+		get,
+		set,
+		remove,
+		update,
+		patch,
+		keys,
+		values,
+		entries,
+		size,
+	}
 }
